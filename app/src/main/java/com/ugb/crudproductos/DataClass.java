f5a09cb61b86867292a9d03fb357a767ebbdc163
Edwin <usss017222@ugb.edu.sv>
2023-06-13 01:37:56 -0600

    cambios

--- app/src/main/java/com/ugb/crudproductos/DataClass.java
+++ app/src/main/java/com/ugb/crudproductos/DataClass.java
@@ -1,9 +1,25 @@
 package com.ugb.crudproductos;
 public class DataClass {
-    private String dataTitle;
-    private String dataDesc;
-    private String dataLang;
-    private String dataImage;
+    private String dataTitle; //nombre
+    private String dataDesc; //descripcion
+    private String dataLang; //Precio
+    private String dataImage; //imagen
+    private String dataMarca; //Marca
+    private String datadisp; //disponibilidad
+    private String dataespecs; //especificaciones
+
+    public String getDataMarca() {
+        return dataMarca;
+    }
+
+    public String getDatadisp() {
+        return datadisp;
+    }
+
+    public String getDataespecs() {
+        return dataespecs;
+    }
+
     private String key;
     public String getKey() {
         return key;
--- app/src/main/java/com/ugb/crudproductos/DetailActivity.java
+++ app/src/main/java/com/ugb/crudproductos/DetailActivity.java
@@ -6,6 +6,7 @@ import android.annotation.SuppressLint;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -21,7 +22,8 @@ import com.google.firebase.storage.StorageReference;
 public class DetailActivity extends AppCompatActivity {
     TextView detailDesc, detailTitle, detailLang;
     ImageView detailImage;
-    FloatingActionButton deleteButton, editButton;
+    Button deleteButton, editButton;
+
     String key = "";
     String imageUrl = "";
     @SuppressLint("MissingInflatedId")
